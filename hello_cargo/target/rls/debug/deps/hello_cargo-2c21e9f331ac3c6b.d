e85ee1c8c263e413fcdaa241704bccfcd7cade37
Aadit <baggaaadit017@gmail.com>
2021-04-15 08:53:49 +0530

    made a new folder

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-2c21e9f331ac3c6b.d
@@ -0,0 +1,5 @@
+/home/aadit/Desktop/coding/rust/hello_cargo/target/rls/debug/deps/hello_cargo-2c21e9f331ac3c6b.rmeta: src/main.rs
+
+/home/aadit/Desktop/coding/rust/hello_cargo/target/rls/debug/deps/hello_cargo-2c21e9f331ac3c6b.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-4198ad5de53aba60.d
@@ -0,0 +1,5 @@
+/home/aadit/Desktop/coding/rust/hello_cargo/target/rls/debug/deps/hello_cargo-4198ad5de53aba60.rmeta: src/main.rs
+
+/home/aadit/Desktop/coding/rust/hello_cargo/target/rls/debug/deps/hello_cargo-4198ad5de53aba60.d: src/main.rs
+
+src/main.rs:
