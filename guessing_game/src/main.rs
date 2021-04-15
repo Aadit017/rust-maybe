a06c097d30bcd010dc572be22f4b259bb9e40993
Aadit <baggaaadit017@gmail.com>
2021-04-15 15:05:08 +0530

    finally made my own rust guess game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,23 +6,27 @@ fn main() {
     println!("Guess the number!");
     //  remember println! is A macro 
     // anything with a ! in the end is a macro 
-    println!("Please input your guess.");
+    
     let a_secret_number=rand::thread_rng().gen_range(1,101); 
-    println!("the secret number = {}", a_secret_number);
-    let mut guess = String::new();
-    // mut means mutable if you remember 
-    // :: means that new is being impolemented on 'String'
-    io::stdin() // is we wldnt have dec above then we wld have written std::io::stdin
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed : {}", guess); 
-    let guess : u32= guess.trim().parse().expect("please enter the variable again ");    
-
-    match guess.cmp(&a_secret_number){
-        Ordering::Less=>println!(" too less"),
-        Ordering::Greater=>println!(" too much "),
-        Ordering::Equal=>println!(" you guessed it rt "),
+    loop{ 
+        println!("Please input your guess.");
+        // println!("the secret number = {}", a_secret_number);
+        let mut guess = String::new();
+        // mut means mutable if you remember 
+        // :: means that new is being impolemented on 'String'
+        io::stdin() // is we wldnt have dec above then we wld have written std::io::stdin
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        println!("You guessed : {}", guess); 
+        let guess : u32= guess.trim().parse().expect("please enter the variable again ");    
+            match guess.cmp(&a_secret_number){
+            Ordering::Less=>println!(" too less"),
+            Ordering::Greater=>println!(" too much "),
+            Ordering::Equal=>{
+                println!(" you guessed it rt ");
+                break; 
+        }}
     }
 
     // let just_another_random_variabl="234";
