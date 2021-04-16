9ea882a02624f6afaf85c46ffffa825d5e78ab6a
Aadit <baggaaadit017@gmail.com>
2021-04-16 12:50:40 +0530

    this is cool too

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,6 +1,8 @@
 fn main(){ 
     let tup:(i32 , u32 ,bool) = (-500 , 34 , true) ;
-    let (x,y,z)=tup; 
+    let x=tup.0 ;
+    let y=tup.1;
+    let z=tup.2;
     println!("{} {} {}",x,y,z );
      } 
 
